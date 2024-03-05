cc687f9c2e357c1eace304cc95b2e89f8abc1482
Sebastien Dupont <sebastiengdupont@gmail.com>
2024-03-04 16:23:59 -0800

    delete unnecessary component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Youtube from './Components/Youtube'
 import GetTittle from './Components/GetTittle';
 
 export default function App() {
