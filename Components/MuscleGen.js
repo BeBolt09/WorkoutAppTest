c4c47b9e323b4e9738377191bfba1d7edb26428f
Tom Garrett <tomgarrett906@gmail.com>
2024-03-26 11:04:34 -0500

    muscle tab style

--- Components/MuscleGen.js
+++ Components/MuscleGen.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import axios from 'axios';
 import { Buffer } from 'buffer';
 
-// Polyfill for Buffer
+
 global.Buffer = global.Buffer || require('buffer').Buffer;
 
 const MuscleGroupImage = () => {
@@ -12,9 +12,17 @@ const MuscleGroupImage = () => {
   const fetchImage = async () => {
     try {
       const response = await axios.get(
-        'https://muscle-group-image-generator.p.rapidapi.com/getImage?muscleGroups=shoulders',
+        'https://muscle-group-image-generator.p.rapidapi.com/getMulticolorImage',
         {
+          params: {
+            primaryColor: '1,228,243', // Adjust primary color as needed
+            secondaryColor: '28,108,128', // Adjust secondary color as needed
+            primaryMuscleGroups: 'chest', // Adjust primary muscle group as needed
+            secondaryMuscleGroups: 'triceps,shoulders', // Adjust secondary muscle groups as needed
+            transparentBackground: '1', // Set to '1' for transparent background
+          },
           headers: {
+            
             'X-RapidAPI-Key': '485940b71bmsh643dd5fbc0bef25p1b23acjsnb16aee981e44',
             'X-RapidAPI-Host': 'muscle-group-image-generator.p.rapidapi.com',
           },
@@ -34,11 +42,13 @@ const MuscleGroupImage = () => {
   }, []);
 
   return image ? (
-    <Image
-      source={{ uri: `data:image/jpeg;base64,${image}` }}
-      style={{ width: 200, height: 200 }} // Adjust dimensions as needed
-      resizeMode="contain" // or other appropriate resizeMode
-    />
+    <View>
+      <Image
+        source={{ uri: `data:image/jpeg;base64,${image}` }}
+        style={{ width: 400, height: 400}}
+        resizeMode="contain"
+      />
+    </View>
   ) : null;
 };
 
