731189238728a87c32d3049763e1f3165d0eebc1
Tom Garrett <tomgarrett906@gmail.com>
2024-03-28 22:30:28 -0500

    removed search screen header

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,21 @@
+//SearchScreen.js
+
 import React, { useState } from "react";
-import { View, TextInput, Platform, StatusBar } from "react-native";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Platform,
+  StatusBar,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { GlobalStyles, SearchScreenStyles } from "../Styles";
+import { FirstScreenStyles, GlobalStyles, SearchScreenStyles } from "../Styles";
+import { Feather } from "@expo/vector-icons";
 
 const SearchScreen = ({ navigation }) => {
   const [inputValue, setInputValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
   const isWeb = Platform.OS === "web";
 
   const handleButtonPress = () => {
@@ -13,10 +24,49 @@ const SearchScreen = ({ navigation }) => {
 
   return (
     <View style={GlobalStyles.rootContainer}>
-      <View style={isWeb ? GlobalStyles.webContainer : GlobalStyles.mobileContainer}>
-        <LinearGradient colors={GlobalStyles.gradient.colors} style={GlobalStyles.gradient}>
+      <View
+        style={
+          isWeb ? FirstScreenStyles.webContainer : GlobalStyles.mobileContainer
+        }
+      >
+        <LinearGradient
+          colors={GlobalStyles.gradient.colors}
+          style={GlobalStyles.gradient}
+        >
           <StatusBar backgroundColor="#313b3f" barStyle="light-content" />
+
           <View style={SearchScreenStyles.body}>
+            <View style={SearchScreenStyles.inputContainer}>
+              <TouchableOpacity
+                onPress={handleButtonPress}
+                style={SearchScreenStyles.searchButton}
+              >
+                <Feather name="search" size={24} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setInputValue("")}
+                style={SearchScreenStyles.XButton}
+              >
+                <Feather name="x" size={28} color="white" />
+              </TouchableOpacity>
+              <TextInput
+                placeholder="Swap exercise"
+                placeholderTextColor="lightgray"
+                style={SearchScreenStyles.input}
+                onChangeText={setInputValue}
+                value={inputValue}
+              />
+              <TouchableOpacity
+                style={SearchScreenStyles.cancelButton}
+                onPress={() =>
+                  navigation.navigate("FourthScreen", {
+                    selectedExercise: inputValue,
+                  })
+                }
+              >
+                <Text style={SearchScreenStyles.cancelButtonText}>CANCEL</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </LinearGradient>
       </View>
